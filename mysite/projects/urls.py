466dc2c6dd19aea230b370cc1e964f9ac64cc075
Donatas Noreika <donoras@gmail.com>
2022-05-09 20:01:37 +0300

    added project.html and project_list.html

---
+++ mysite/projects/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.ProjectListView.as_view(), name="projects"),
+    path('<int:pk>', views.ProjectDetailView.as_view(), name="project"),
+]
--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Project
+
 
 # Create your views here.
+class ProjectListView(generic.ListView):
+    model = Project
+    template_name = 'project_list.html'
+    context_object_name = 'projects'
+
+
+class ProjectDetailView(generic.DetailView):
+    model = Project
+    template_name = 'project.html'
+    context_object_name = 'project'
