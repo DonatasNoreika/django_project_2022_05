201f07d75d4f563316453f0a6e438c0f907aa4fd
Donatas Noreika <donoras@gmail.com>
2022-05-09 20:19:17 +0300

    added registration

--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Project
-
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
 
 # Create your views here.
 class ProjectListView(generic.ListView):
@@ -14,3 +16,32 @@ class ProjectDetailView(generic.DetailView):
     model = Project
     template_name = 'project.html'
     context_object_name = 'project'
+
+
+
+@csrf_protect
+def register(request):
+    if request.method == "POST":
+        # pasiimame reikšmes iš registracijos formos
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # tikriname, ar sutampa slaptažodžiai
+        if password == password2:
+            # tikriname, ar neužimtas username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, f'Vartotojo vardas {username} užimtas!')
+                return redirect('register')
+            else:
+                # tikriname, ar nėra tokio pat email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
+                    return redirect('register')
+                else:
+                    # jeigu viskas tvarkoje, sukuriame naują vartotoją
+                    User.objects.create_user(username=username, email=email, password=password)
+        else:
+            messages.error(request, 'Slaptažodžiai nesutampa!')
+            return redirect('register')
+    return render(request, 'registration/register.html')
